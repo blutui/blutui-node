b3d87cd81aabd73ac83463237631dad9b7508b3f
Jayan Ratna <jayan@blutui.com>
2024-05-09 09:29:49 +1200

    add brand fixtures

--- src/resources/agency/brand/brand.spec.ts
+++ src/resources/agency/brand/brand.spec.ts
@@ -3,6 +3,8 @@ import fetch from 'jest-fetch-mock'
 import { Blutui } from '@/blutui'
 import { fetchOnce, fetchURL } from '@/utils/testing'
 
+import brandFixture from './fixtures/brand.json'
+
 const accessToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 const blutui = new Blutui(accessToken)
@@ -12,7 +14,7 @@ describe('Brand', () => {
 
   describe('get', () => {
     it('can get the agency brand', async () => {
-      fetchOnce({ object: 'brand' })
+      fetchOnce(brandFixture)
       const brand = await blutui.agency('foo').brand.get()
 
       expect(fetchURL()).toContain('/v1/agencies/foo/brand')
@@ -24,34 +26,28 @@ describe('Brand', () => {
 
   describe('create', () => {
     it('can create a new agency brand', async () => {
-      fetchOnce({
-        id: '9bfdb42b-1bf0-4510-978e-46aa329f8efa',
-        object: 'brand',
-        logo: null,
-        primary_color: '#000000',
-        secondary_color: '#222222',
-      })
+      fetchOnce(brandFixture)
       const brand = await blutui.agency('foo').brand.create({
-        primaryColor: '#000000',
-        secondaryColor: '#222222',
+        primaryColor: '#6227FF',
+        secondaryColor: '#333333',
       })
 
       expect(fetchURL()).toContain('/v1/agencies/foo/brand')
       expect(brand).toMatchObject({
         id: '9bfdb42b-1bf0-4510-978e-46aa329f8efa',
         object: 'brand',
-        logo: null,
-        primaryColor: '#000000',
-        secondaryColor: '#222222',
+        logo: 'https://cdn.blutui.com/uploads/assets/logo/youragency.svg',
+        primaryColor: '#6227FF',
+        secondaryColor: '#333333',
       })
     })
   })
 
   describe('update', () => {
     it('can update an agency brand', async () => {
-      fetchOnce({ object: 'brand' })
+      fetchOnce(brandFixture)
       const brand = await blutui.agency('foo').brand.update({
-        primaryColor: '#000000',
+        primaryColor: '#6227FF',
       })
 
       expect(fetchURL()).toContain('/v1/agencies/foo/brand')
